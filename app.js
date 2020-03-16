c551b76d969ae30267f82c3fe36bdd953366b342
Adam-Harte <adamharte11@hotmail.co.uk>
2020-03-16 23:58:51 +0000

    adding, modifying and deleting properties added

---
+++ app.js
@@ -0,0 +1,24 @@
+const person = {
+  name: 'Adam',
+  age: 27,
+  hobbies: ['tennis', 'reading'],
+  greet: function() {
+    alert('Hi there!');
+  }
+};
+
+console.log(person);
+
+//adding properties
+person.isAdmin = true;
+console.log(person);
+
+//modifying properties
+person.age = 28;
+console.log(person);
+
+//deleting properties
+delete person.age;
+person.greet = undefined; //not recommended to assing properties/variables to undefined
+person.hobbies = null;
+console.log(person);
